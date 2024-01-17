a1671e9eb7afbaee11a463bd458d6871de31f2e5
Dinesh <varaldinesh0@gmail.com>
2024-01-17 16:23:13 +0530

    Database Connection

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
+const { MongoClient } = require('mongodb');
 
+const app = express();
 app.use(express.json());
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
-let todos = []; // This will act as a simple in-memory database
+const mongoUri = process.env.MONGODB_URI;
 
-// GET - Read all todos
-app.get('/todos', (req, res) => {
-    res.json(todos);
-});
+const client = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// POST - Create a new todo
-app.post('/todos', (req, res) => {
-    const todo = {
-        id: Date.now(), // simple ID generation
-        text: req.body.text,
-    };
-    todos.push(todo);
-    res.status(201).json(todo);
-});
 
-app.delete('/todos/:id', (req, res) => {
-    todos = todos.filter(t => t.id !== parseInt(res.params.id));
-    res.status(204).send()
-})
+async function main() {
+
+    try {
+
+        await client.connect();
+        console.log('Connected to MongoDB Atlas');
+        const db = client.db();
+
+    } catch (error) {
+
+        console.error(error);
+
+    } finally {
+        await client.close();
+    }
+}
