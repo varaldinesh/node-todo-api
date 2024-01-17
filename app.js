3a0a8d94674a641aab1fe4bc2c0780f1f3b5f828
Dinesh <varaldinesh0@gmail.com>
2024-01-17 11:02:18 +0530

    Add Todo

--- app.js
+++ app.js
@@ -6,3 +6,20 @@ app.use(express.json());
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+let todos = []; // This will act as a simple in-memory database
+
+// GET - Read all todos
+app.get('/todos', (req, res) => {
+    res.json(todos);
+});
+
+// POST - Create a new todo
+app.post('/todos', (req, res) => {
+    const todo = {
+        id: Date.now(), // simple ID generation
+        text: req.body.text,
+    };
+    todos.push(todo);
+    res.status(201).json(todo);
+});
+
